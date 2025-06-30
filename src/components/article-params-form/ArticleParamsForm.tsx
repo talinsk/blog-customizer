2a8c0f241ad5f63fb6ffd5c9f0ddfc0769a16bb4
Natalia <tali154@yandex.ru>
2025-06-30 20:35:34 +0700

    добавлены компоненты на форму панели и состояние панели, исправлены стили в соответствии с макетом

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,23 +1,47 @@
+import { useState } from 'react';
+import { Text } from 'src/ui/text';
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
-import { useState } from 'react';
 import { Select } from 'src/ui/select';
-import { fontFamilyOptions, OptionType } from 'src/constants/articleProps';
-
+import { RadioGroup } from 'src/ui/radio-group';
+import { Separator } from 'src/ui/separator';
+import {
+	fontFamilyOptions,
+	fontSizeOptions,
+	fontColors,
+	backgroundColors,
+	contentWidthArr,
+	OptionType,
+} from 'src/constants/articleProps';
 import styles from './ArticleParamsForm.module.scss';
 
 type ArticleParamsFormState = {
 	isOpen: boolean;
 	selectedFontOption: OptionType;
+	selectedFontSizeOption: OptionType;
+	selectedFontColorsOption: OptionType;
+	selectedBackgroundColorsOption: OptionType;
+	selectedContentWidthArrOption: OptionType;
 };
 
 export const ArticleParamsForm = () => {
 	const [state, setState] = useState<ArticleParamsFormState>({
 		isOpen: true,
 		selectedFontOption: fontFamilyOptions[0],
+		selectedFontSizeOption: fontSizeOptions[0],
+		selectedFontColorsOption: fontColors[0],
+		selectedBackgroundColorsOption: backgroundColors[0],
+		selectedContentWidthArrOption: contentWidthArr[0],
 	});
 
-	const { isOpen, selectedFontOption } = state;
+	const {
+		isOpen,
+		selectedFontOption,
+		selectedFontSizeOption,
+		selectedFontColorsOption,
+		selectedBackgroundColorsOption,
+		selectedContentWidthArrOption,
+	} = state;
 
 	return (
 		<>
@@ -29,7 +53,9 @@ export const ArticleParamsForm = () => {
 			/>
 			<aside className={isOpen ? styles.container_open : styles.container}>
 				<form className={styles.form}>
-					<h1>Задайте параметры</h1>
+					<Text as='h1' size={31} weight={800} uppercase dynamicLite>
+						Задайте параметры
+					</Text>
 					<Select
 						options={fontFamilyOptions}
 						selected={selectedFontOption}
@@ -38,6 +64,41 @@ export const ArticleParamsForm = () => {
 							setState({ ...state, selectedFontOption: opt });
 						}}
 					/>
+					<RadioGroup
+						name='font-size'
+						title='Размер шрифта'
+						options={fontSizeOptions}
+						selected={selectedFontSizeOption}
+						onChange={(opt) => {
+							setState({ ...state, selectedFontSizeOption: opt });
+						}}
+					/>
+					<Select
+						options={fontColors}
+						selected={selectedFontColorsOption}
+						title='Цвет шрифта'
+						onChange={(opt) => {
+							setState({ ...state, selectedFontColorsOption: opt });
+						}}
+					/>
+					<Separator />
+					<Select
+						options={backgroundColors}
+						selected={selectedBackgroundColorsOption}
+						title='Цвет фона'
+						onChange={(opt) => {
+							setState({ ...state, selectedBackgroundColorsOption: opt });
+						}}
+					/>
+					<Select
+						options={contentWidthArr}
+						selected={selectedContentWidthArrOption}
+						title='Ширина контента'
+						onChange={(opt) => {
+							setState({ ...state, selectedContentWidthArrOption: opt });
+						}}
+					/>
+
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
