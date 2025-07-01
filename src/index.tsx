7cf4c6a6ff74a2ef15b35c4729d793e745787f5e
Natalia <tali154@yandex.ru>
2025-07-02 00:48:26 +0700

    компонент App вынесен в отдельный файл

--- src/index.tsx
+++ src/index.tsx
@@ -1,45 +1,12 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties, useState } from 'react';
+import { StrictMode } from 'react';
 
-import { Article } from './components/article/Article';
-import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import {
-	ArticleStateType,
-	defaultArticleState,
-} from './constants/articleProps';
+import { App } from './components/app/App';
 
 import './styles/index.scss';
-import styles from './styles/index.module.scss';
 
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
-
-const App = () => {
-	const [state, setState] = useState<ArticleStateType>({
-		...defaultArticleState,
-	});
-
-	const handleParamsApplied = (pars: ArticleStateType) => {
-		setState(pars);
-	};
-
-	return (
-		<main
-			className={styles.main}
-			style={
-				{
-					'--font-family': state.fontFamilyOption.value,
-					'--font-size': state.fontSizeOption.value,
-					'--font-color': state.fontColor.value,
-					'--container-width': state.contentWidth.value,
-					'--bg-color': state.backgroundColor.value,
-				} as CSSProperties
-			}>
-			<ArticleParamsForm onApply={handleParamsApplied} />
-			<Article />
-		</main>
-	);
-};
 
 root.render(
 	<StrictMode>
